856ba839eaa03ba999c37dcacbcbf9cfd4ebcd5c
Boray Chen <r0975929562@gmail.com>
2025-04-13 16:53:09 +0800

    Initial Commit

---
+++ CardWallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardWallet
+//
+//  Created by Boray Chen on 2025/4/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CardWalletTests/CardWalletTests.swift
@@ -0,0 +1,17 @@
+//
+//  CardWalletTests.swift
+//  CardWalletTests
+//
+//  Created by Boray Chen on 2025/4/13.
+//
+
+import Testing
+@testable import CardWallet
+
+struct CardWalletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
